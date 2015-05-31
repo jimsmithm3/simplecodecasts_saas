cd64a191e448dd1e71dbbe31bb283c3340369f35
jim <jimsmithm3@gmail.com>
2015-05-31 23:42:36 +0000

    css

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150531221011) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "plan_id"
+    t.string   "token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
